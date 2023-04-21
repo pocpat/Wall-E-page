97df3874491eb255d8c45e4fd94cc31c0fc9ea50
Elena Kroupkin <pocoat@gmail.com>
2023-04-21 14:01:07 +1200

    section2 and footer are ready

--- src/homePageComponents/Section2.js
+++ src/homePageComponents/Section2.js
@@ -1,21 +1,23 @@
 import React from 'react'
 import './Section2.css'
 import { CardSection2 } from './CardSection2'
-
+import robot1 from '../imgs/robot1.png'
+import robot2 from '../imgs/robot2.png'
+import robot3 from '../imgs/robot3.png'
 const Section2 = () => {
   return (
     <div className='bodyContainerS2'>
   
     <CardSection2 
-    imgCardS2Src="#"
+    imgCardS2Src={robot1}
     titleS2="Lorem Ipsum"
     textS2="Ex ea commodo"/>
     <CardSection2
-    imgCardS2Src="#"
+    imgCardS2Src={robot2}
     titleS2="Duis aute"
     textS2="Quis nostrud"/>
     <CardSection2
-    imgCardS2Src="#"
+    imgCardS2Src={robot3}
     titleS2="Excepteur sint"
     textS2="Cillum dolore"/>
     </div>
