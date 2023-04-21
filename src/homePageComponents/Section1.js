8129974ff568a2552e45e203da3b51254ed36963
Elena Kroupkin <pocoat@gmail.com>
2023-04-21 13:47:50 +1200

    section 1 colors

--- src/homePageComponents/Section1.js
+++ src/homePageComponents/Section1.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import './Section1.css'
 import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
 
 const Section1 = () => {
   return (
-    <div className='bodySection1'>
+    <Box sx={{ flexGrow: 1 }}>
+    <div className='bodySection1 bgImgS1'
+    >
 
     {/* titleS1 is the container of title only */}
     <div className='titleS1'>
@@ -14,15 +17,35 @@ const Section1 = () => {
 
     {/* inputS1 is the container of input and button */}
     <div className='inputS1'>
-    <input className='userInputS1' type='text' placeholder='adipiscing elit' />
+    <input 
+    className='userInputS1' 
+    type='text' 
+    placeholder='adipiscing elit' 
+    sx={{
+      backgroundColor: "rgb (228, 250, 233)",
+      color: "rgb (48, 111, 85)",
+      outline: "rgb(58, 112, 56)",
+    }}
+    
+    
+    />
   { /* <button className='btnS1'>Ullamco </button> */}
-    <Button  sx={{ color: '#fff', bgcolor: 'grey.900', minWidth:"100px" }}>Ullamco </Button>
+    <Button className='btnS1' 
+    sx={{
+      minWidth: "100px",
+      backgroundColor: "rgb(58, 112, 56)",
+      color: "rgb(255, 255, 255)",
+      borderRadius: "0 6px 6px 0",
+    }}
+    
+    >Ullamco </Button>
    
 
     </div>
 
 
     </div>
+    </Box>
   )
 }
 
