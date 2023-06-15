e9c9fd3eefdf2b73b6e5b33a9d75739f299dacd7
Elena Kroupkin <pocoat@gmail.com>
2023-06-15 13:06:19 +1200

    Initial commit

---
+++ src/homePageComponents/test.js
@@ -0,0 +1,25 @@
+"options": [
+    {
+      "link": "https://www.turners.co.nz",
+      "image": {
+        "src": {
+          "rawUrl": "https://example.com/image2.png"
+        }
+      },
+
+      "text": "Turners cars"
+},
+
+
+
+{
+    "link":"https://www.trademe.co.nz",
+    "image":{
+        "src":{
+            "rawUrl":"https://example.com/image2.png"
+        }
+    },
+"text":"Trade Me" 
+
+}
+  ],
